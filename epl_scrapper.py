c266252e5a1e5bae77b7beea864e48a3a096eb86
SevkavTV <seva.archakov@gmail.com>
2022-01-01 16:58:21 +0200

    changed structure

--- epl_scrapper.py
+++ epl_scrapper.py
@@ -1,12 +1,57 @@
 import requests
 import pandas as pd
 
-NUMBER_OF_MATCHES = 5
+NUMBER_OF_MATCHES = 380
+TEAMS = [
+    1,      # Arsenal
+    2,      # Aston Villa
+    131,    # Brighton
+    43,     # Burnley
+    4,      # Chelsea
+    6,      # Crystal Palace
+    7,      # Everton
+    34,     # Fulham
+    9,      # Leeds United
+    26,     # Leicester City
+    10,     # Liverpool
+    11,     # Manchester City
+    12,     # Manchester United
+    23,     # Newcastle United
+    18,     # Sheffield United
+    20,     # Southhampton
+    21,     # Totenham Hotpurs
+    36,     # West Bronwich Albion
+    25,     # West Ham United
+    38      # Wolverhampton Wanderers
+    ]
 SEASON_20_21_URL = f'https://footballapi.pulselive.com/football/fixtures?comps=1&compSeasons=363&teams=1,2,131,43,4,6,7,34,9,26,10,11,12,23,18,20,21,36,25,38&page=0&pageSize={NUMBER_OF_MATCHES}&sort=desc&statuses=C&altIds=true'
 MATCH_STATISTICS_URL = "https://footballapi.pulselive.com/football/stats/match"
 
 headers = {"origin": "https://www.premierleague.com"}
-use_stats_names = ['formation_used', 'touches', 'total_pass', 'total_tackle']
+use_stats_names = ['formation_used',
+
+                    ### Overall ###
+                    'touches',
+                    'total_pass',
+                    "long_pass_own_to_opp_success",
+
+                    ### Agressive ###
+                    "pen_area_entries",
+                    "final_third_entries",
+                    "total_fwd_zone_pass",
+                    "total_final_third_passes",
+                    "attempts_obox", # outside box
+                    "attempts_ibox", # inside box
+                    "fouled_final_third",
+                    "total_offside",
+
+                    ### defensive ###
+                    'total_tackle',
+                    "blocked_pass",
+                    "interception",
+                    "total_back_zone_pass",
+                    "fk_foul_lost", # team fouled, freekick against them
+                    ]
 
 
 def get_all_matches(season_url: str):
@@ -17,7 +62,7 @@ def get_all_matches(season_url: str):
         match_id = int(match['id'])
         match_info = get_match_statistics(f'{MATCH_STATISTICS_URL}/{match_id}')
         data.append(match_info)
-    
+
     return data
 
 
@@ -56,8 +101,16 @@ def create_csv(matches):
         final_match_data = match[0]
         stats_result = match[1]
         for stat_name in use_stats_names:
-            final_match_data.extend([stats_result[stat_name][0],
-                            stats_result[stat_name][1]])
+
+            try:
+                final_match_data.extend([stats_result[stat_name][0]])
+            except IndexError:
+                final_match_data.extend([0])
+            try:
+                final_match_data.extend([stats_result[stat_name][1]])
+            except IndexError:
+                final_match_data.extend([0])
+
         final_data.append(final_match_data)
 
     df = pd.DataFrame(final_data, columns=csv_columns)
