6d2ca1eeba76cb7056b6fdf8a345d666c4e20ab3
SevkavTV <seva.archakov@gmail.com>
2022-01-01 18:00:03 +0200

    changed number of matches

--- transfrom_csv.py
+++ transfrom_csv.py
@@ -1,39 +1,50 @@
 import pandas as pd
 
+COL_NAMES = [
+
+    ### Overall ###
+    'team',
+    'formation_used',
+    'touches',
+    'total_pass',
+    "long_pass_own_to_opp_success",
+
+    ### Agressive ###
+    "pen_area_entries",
+    "final_third_entries",
+    "total_fwd_zone_pass",
+    "total_final_third_passes",
+    "attempts_obox",  # outside box
+    "attempts_ibox",  # inside box
+    "total_offside",
+
+    ### defensive ###
+    'total_tackle',
+    "blocked_pass",
+    "interception",
+    "total_back_zone_pass",
+    "fk_foul_lost",  # team fouled, freekick against them
+]
+
+
 def wtf(f_name):
     df = pd.read_csv(f_name)
     d = dict()
     team_lst = df["team1"]
     team_lst = list(set(team_lst))
     for team_name in team_lst:
-        df_team = df[df["team1"] == team_name]
-        df_team = df_team[[
+        df_team1 = df[df["team1"] == team_name]
+        df_team2 = df[df["team2"] == team_name]
+        team1_cols = [x + "1" for x in COL_NAMES]
+        team2_cols = [x + "2" for x in COL_NAMES]
+        df_team1 = df_team1[team1_cols]
+        df_team2 = df_team2[team2_cols]
+        df_team1.columns = COL_NAMES
+        df_team2.columns = COL_NAMES
+        df_team = pd.concat([df_team1, df_team2])
 
-                    ### Overall ###
-                    'team1',
-                    'formation_used1',
-                    'touches1',
-                    'total_pass1',
-                    "long_pass_own_to_opp_success1",
-
-                    ### Agressive ###
-                    "pen_area_entries1",
-                    "final_third_entries1",
-                    "total_fwd_zone_pass1",
-                    "total_final_third_passes1",
-                    "attempts_obox1", # outside box
-                    "attempts_ibox1", # inside box
-                    "fouled_final_third1",
-                    "total_offside1",
-
-                    ### defensive ###
-                    'total_tackle1',
-                    "blocked_pass1",
-                    "interception1",
-                    "total_back_zone_pass1",
-                    "fk_foul_lost1", # team fouled, freekick against them
-                    ]]
         df_team.to_csv(f'teams/{team_name}_stats.csv', index=False)
+
 
 if __name__ == "__main__":
     wtf("matches.csv")
